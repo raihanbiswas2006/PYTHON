d934b0318174c5fe62edd701b16fbdea37a1dd2d
raihanbiswas2006 <raihanbiswas2006@gmail.com>
2026-01-14 00:02:06 +0600

    Comment

---
+++ comment.py
@@ -0,0 +1,19 @@
+a = int(input("Enter the first number : "))
+b = int(input("Enter the second number : "))
+
+print("The average of given two numbers is :" , (a+b)/2)
+
+# ctrl + / it make single line comment
+print(
+    "This is single line comment example"
+)
+""" if
+    we 
+    use
+    it
+    it means
+    it
+    is
+    multiple
+    line
+    comment"""
